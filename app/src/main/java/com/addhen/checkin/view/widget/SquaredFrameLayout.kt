07949912a37e49db32edad352a06c24e2c443411
Henry Addo <henry@addhen.org>
2018-03-11 19:15:57 +0100

    Fix lint issues

--- app/src/main/java/com/addhen/checkin/view/widget/SquaredFrameLayout.kt
+++ app/src/main/java/com/addhen/checkin/view/widget/SquaredFrameLayout.kt
@@ -21,11 +21,13 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
 
-class SquaredFrameLayout(
-    context: Context,
-    attrs: AttributeSet,
-    defStyleAttr: Int,
-    defStyleRes: Int) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
+class SquaredFrameLayout : FrameLayout {
+
+  constructor(context: Context) : super(context)
+
+  constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+  constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
   override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
     super.onMeasure(widthMeasureSpec, widthMeasureSpec)
