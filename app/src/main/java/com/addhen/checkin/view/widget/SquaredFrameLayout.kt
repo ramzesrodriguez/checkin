52d1162984f255fc45b2969d3f4958aa8926ba4a
Henry Addo <henry@addhen.org>
2018-03-11 19:25:44 +0100

    Fix code violations

--- app/src/main/java/com/addhen/checkin/view/widget/SquaredFrameLayout.kt
+++ app/src/main/java/com/addhen/checkin/view/widget/SquaredFrameLayout.kt
@@ -27,7 +27,8 @@ class SquaredFrameLayout : FrameLayout {
 
   constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
-  constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+  constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs,
+      defStyleAttr)
 
   override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
     super.onMeasure(widthMeasureSpec, widthMeasureSpec)
